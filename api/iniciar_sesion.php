91734a76990637b5816a18f90b95c58a98ec2eba
chapaxD <alexandiacoc2@gmail.com>
2026-04-04 01:34:27 -0400

    debug: capturar errores PHP como JSON en iniciar_sesion

--- api/iniciar_sesion.php
+++ api/iniciar_sesion.php
@@ -1,4 +1,10 @@
 <?php
+set_exception_handler(function($e) {
+    http_response_code(500);
+    header('content-type: application/json; charset=utf-8');
+    echo json_encode(['error' => $e->getMessage(), 'file' => basename($e->getFile()), 'line' => $e->getLine()]);
+    exit;
+});
 
 $usuario = json_decode(file_get_contents("php://input"));
 if(!$usuario) exit("No se encontraron datos");

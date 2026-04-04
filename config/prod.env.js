d6b3502c64f193c64a2ce0c75695577c931e6ebf
chapaxD <alexandiacoc2@gmail.com>
2026-04-03 23:54:11 -0400

    fix: API_BASE relativo para proxy Vercel

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_BASE: '"https://lalumbrera.infinityfreeapp.com/api/"',
+  API_BASE: '"/api/"',
   SSE_BASE: '"https://lalumbrera.infinityfreeapp.com/api/"'
 }

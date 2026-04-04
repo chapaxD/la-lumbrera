a28233f473540af0be77976a48d57ff730ce63e5
chapaxD <alexandiacoc2@gmail.com>
2026-04-04 01:43:08 -0400

    debug: capturar errores PHP como JSON en inicio.php

--- api/inicio.php
+++ api/inicio.php
@@ -1,4 +1,10 @@
 <?php
+set_exception_handler(function($e) {
+    http_response_code(500);
+    header('content-type: application/json; charset=utf-8');
+    echo json_encode(['error' => $e->getMessage(), 'file' => basename($e->getFile()), 'line' => $e->getLine()]);
+    exit;
+});
 include_once "encabezado.php";
 include_once "funciones.php";
 

e2c6836903815a59983a921167ff42e7cc04b977
chapaxD <alexandiacoc2@gmail.com>
2026-04-17 21:46:09 -0400

    Configuracion para Render Virginia

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_BASE: '"/api/"',
+  API_BASE: '"https://la-lumbrera-resto.onrender.com/api/"',
   SSE_BASE: '"https://la-lumbrera-resto.onrender.com/api/"'
 }
